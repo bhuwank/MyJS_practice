aa4ac54a548394a4d16498182568fb7e3c6a921a
Bhuwan kumar <bhuwak53@gamil.com>
2025-10-26 22:20:08 +0530

    first commit

---
+++ Desktop/SpeedRecovery/02 New Volume (E ) Delete file  (195 files)/projects/javaScript/api/projects/country-data/script.js
@@ -0,0 +1,13 @@
+
+const cardContainer = document.querySelector('.card-container');
+cardContainer.innerHTML = `<div class="card">
+            <a class="link" title="link">
+                <img class="flag" src="https://flagcdn.com/w320/fr.png" alt="France Flag" />
+                <div class="card-content">
+                    <h2>France</h2>
+                    <p><strong>Capital:</strong> Paris</p>
+                    <p><strong>Region:</strong> Europe</p>
+                    <p><strong>Population:</strong> 67 million</p>
+                </div>
+            </a>
+        </div>`;
